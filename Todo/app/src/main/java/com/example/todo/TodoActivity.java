154f5de6321281560240b5ff58e911ba1140d16e
nkamaris <nickkamaris@outlook.com>
2018-10-04 14:31:48 +0100

    Lab
    First lab up to copile and run time errors

--- Todo/app/src/main/java/com/example/todo/TodoActivity.java
+++ Todo/app/src/main/java/com/example/todo/TodoActivity.java
@@ -1,13 +1,60 @@
 package com.example.todo;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TodoActivity extends AppCompatActivity {
 
+    private String[] mTodos;
+    private int mTodoIndex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // call the super class onCreate to complete the creation of activity like
+        // the view hierarchy
         super.onCreate(savedInstanceState);
+
+        // set the user interface layout for this Activity
+        // the layout file is defined in the project res/layout/activity_todo.xml file
         setContentView(R.layout.activity_todo);
+
+        // initialize member TextView so we can manipulate it later
+        final TextView TodoTextView;
+        TodoTextView = (TextView) findViewById(R.id.textViewTodo);
+
+        // read the todo array from res/values/strings.xml
+        Resources res = getResources();
+        mTodos = res.getStringArray(R.array.todo);
+        // display the first task from mTodo array in the TodoTextView
+        TodoTextView.setText(mTodos[mTodoIndex]);
+
+        Button buttonNext;
+        buttonNext = (Button) findViewById(R.id.buttonNext);
+
+        buttonNext.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+    /*
+    BUG!
+    Compile time error: mTodoIndexx is misspelled
+    Runtime Error: no check for maximum number of items in the todos array
+
+    // Bug fix compile error, Correct spelling mTodoIndex and not mTodoIndexx
+    // Bug fix run time error, use the remainder as index to the array, i.e.
+    //      mTodoIndex = (mTodoIndex + 1) % todos.length;
+    */
+
+                mTodoIndex = (mTodoIndex + 1) % mTodos.length;
+                TodoTextView.setText(mTodos[mTodoIndex]);
+            }
+        });
     }
 }
